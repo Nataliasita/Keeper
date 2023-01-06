74124532890d49a3e4395c0d56b4f4d3bc0076d8
Natalia Gonzalez <73859972+Nataliasita@users.noreply.github.com>
2023-01-06 10:20:55 -0500

    init level 3

---
+++ Assets/Scripts/Level3/AnimMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimMaterial : MonoBehaviour
+{
+    //Variables
+    [Header("Movimiento")]
+    public float desplazarX = 0.1f;
+    public float desplazarY = 0.1f;
+
+    void Update()
+    {
+        float offsetX = Time.time * desplazarX / 100;
+        float offsetY = Time.time * desplazarY / 100; 
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
---
+++ Assets/Scripts/Level3/UnderWater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderWater : MonoBehaviour
+{
+    public GameObject sistPartBurbujas, vidrioVFX;
+
+    public bool underWater;
+
+    void Update()
+    {
+        if (underWater)
+        {
+            sistPartBurbujas.SetActive(true);
+            RenderSettings.fog = true;
+            RenderSettings.fogDensity = 0.01f;
+            vidrioVFX.SetActive(true);
+        }
+        else
+        {
+            sistPartBurbujas.SetActive(false);
+            RenderSettings.fog = false;
+            vidrioVFX.SetActive(false);
+        }
+    }
+}
